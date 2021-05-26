d7773153f63f13e2906c93eb8bb026449eeda406
jinju <gg66477@gmail.com>
2021-05-26 23:58:17 +0900

    spotify api

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import base64
+import json
+
+client_id = ""
+client_secret = ""
+endpoint = "https://accounts.spotify.com/api/token"
+
+encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
+
+headers = {"Authorization": "Basic {}".format(encoded)}
+payload = {"grant_type": "client_credentials"}
+
+response = requests.post(endpoint, data=payload, headers=headers)
+access_token = json.loads(response.text)['access_token']
+
+print(access_token)
