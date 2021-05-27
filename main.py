f8321f63b8a00968d45cc06735d5683169e96f11
jinju <gg66477@gmail.com>
2021-05-27 18:00:31 +0900

    add:spotify api-Search

--- main.py
+++ main.py
@@ -2,15 +2,35 @@ import requests
 import base64
 import json
 
-client_id = ""
-client_secret = ""
-endpoint = "https://accounts.spotify.com/api/token"
+client_id = "c87e807943a1483883faaaa881aa43ef"
+client_secret = "110de254602e440ea1f72f08f8396ccc"
 
-encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
+def get_token(client_id,client_secret):
 
-headers = {"Authorization": "Basic {}".format(encoded)}
-payload = {"grant_type": "client_credentials"}
+    endpoint = "https://accounts.spotify.com/api/token"
 
-response = requests.post(endpoint, data=payload, headers=headers)
-access_token = json.loads(response.text)['access_token']
+    encoded = base64.b64encode("{}:{}".format(client_id, client_secret).encode('utf-8')).decode('ascii')
 
+    headers = {"Authorization": "Basic {}".format(encoded)}
+    payload = {"grant_type": "client_credentials"}
+
+    response = requests.post(endpoint, data=payload, headers=headers)
+    print(json.loads(response.text))
+    access_token = json.loads(response.text)['access_token']
+
+    return access_token
+
+def search(token):
+    endpoint = "https://api.spotify.com/v1/search"
+
+    headers = {"Authorization": "Bearer  {}".format(token)}
+    query_params = {'q':'BTS','type':'album','limit':5}
+
+    r=requests.get(endpoint,params=query_params,headers=headers)
+    print(json.loads(r.text))
+
+def main():
+    token = get_token(client_id,client_secret)
+    search(token)
+
+main()
