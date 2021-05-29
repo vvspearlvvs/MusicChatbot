d46b826d149fb5843eb7980559ca0b0ce003f61e
jinju <gg66477@gmail.com>
2021-05-29 23:55:12 +0900

    update:SearchAPI-artist

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import requests
 import base64
 import json
 
-import search_artist
+import Search
 
-client_id = ""
-client_secret = ""
+client_id = "c87e807943a1483883faaaa881aa43ef"
+client_secret = "110de254602e440ea1f72f08f8396ccc"
 
 endpoint = "https://accounts.spotify.com/api/token"
 
@@ -22,7 +22,7 @@ access_token = json.loads(response.text)['access_token']
 headers = {"Authorization": "Bearer  {}".format(access_token)}
 
 #1.search artist (with search api)
-search_data = search_artist.search('BTS',headers)['artists']
+search_data = Search.search('BTS', headers)['artists']
 for item in search_data['items']:
     #print(item.keys())
     id = item['id']
