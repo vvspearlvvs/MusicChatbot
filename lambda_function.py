1c9e374d9f2df5c74012c19ea9cf14ebada42692
jinju <gg66477@gmail.com>
2021-06-19 03:52:27 +0900

    final:lambda test

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,7 @@ client_secret = ""
 
 rds_host ='localhost' #RDS로 변경시 Public endpoint
 rds_user ='root' #RDS로 변경시 admin
-rds_pwd = ''
+rds_pwd = 'qwer1234'
 rds_db = 'musicdb'
 
 conn = pymysql.connect(host=rds_host, user=rds_user, password=rds_pwd, db=rds_db)
@@ -20,8 +20,6 @@ cursor = conn.cursor()
 
 dynamodb = boto3.resource(
     'dynamodb',
-    aws_access_key_id='',
-    aws_secret_access_key='',
     region_name='ap-northeast-2'
     )
 table=dynamodb.Table('artist_toptracks') #dynanoDB 파티션키 : track_id
@@ -44,30 +42,31 @@ def insert_row(cursor,data,table):
     #mysql insert
 
     #data의 개수에 맞게 넣어 줌
-     placeholders = ', '.join(['%s'] * len(data)) # 형태: '%s, %s, %s, ...'
-     columns = ', '.join(data.keys())
-     key_placeholders = ', '.join(['{0}=values({0})'.format(k) for k in data.keys()])
+    placeholders = ', '.join(['%s'] * len(data)) # 형태: '%s, %s, %s, ...'
+    columns = ', '.join(data.keys())
+    key_placeholders = ', '.join(['{0}=values({0})'.format(k) for k in data.keys()])
 
-     sql = "INSERT INTO %s ( %s ) VALUES ( %s ) ON DUPLICATE KEY UPDATE %s" % (table, columns, placeholders, key_placeholders)
+    sql = "INSERT INTO %s ( %s ) VALUES ( %s ) ON DUPLICATE KEY UPDATE %s" % (table, columns, placeholders, key_placeholders)
 
-     #print(sql) # 아래와 같은 형태
-     '''
-     INSERT INTO artists ( artist_id, artist_name, followers, popularity, artist_url, image_url )
-     VALUES ( %s, %s, %s, %s, %s, %s )
-     ON DUPLICATE KEY UPDATE artist_id=values(artist_id), artist_name=values(artist_name), followers=values(followers),
-     popularity=values(popularity), artist_url=values(artist_url), image_url=values(image_url)
-     '''
-     cursor.execute(sql, list(data.values()))
+    #print(sql) # 아래와 같은 형태
+    '''
+    INSERT INTO artists ( artist_id, artist_name, followers, popularity, artist_url, image_url )
+    VALUES ( %s, %s, %s, %s, %s, %s )
+    ON DUPLICATE KEY UPDATE artist_id=values(artist_id), artist_name=values(artist_name), followers=values(followers),
+    popularity=values(popularity), artist_url=values(artist_url), image_url=values(image_url)
+    '''
+    cursor.execute(sql, list(data.values()))
 #    print(data.values())
 
 
 #카톡챗봇 메세지 형식 함수
-def response_select(name,followers,popularity,artist_url,image_url):
-    #2.basic 카드형 : 처음에 아티스트 관련 정보만
+def response_select(name,followers,popularity,artist_url,image_url,track_result):
+    youtube_url = 'https://www.youtube.com/results?search_query={}'.format(name.replace(' ', '+'))
     result={
         "version": "2.0",
         "template": {
             "outputs": [
+                #2.basic 카드형 : DB에 있는 아티스트 입력했을 때 메세지
                 {
                     "basicCard": {
                         "title": name,
@@ -78,25 +77,39 @@ def response_select(name,followers,popularity,artist_url,image_url):
                         "buttons": [
                             {
                                 "action":  "webLink",
-                                "label": "Spotify에서 확인하기",
+                                "label": "Spotify에서 검색하기",
                                 "webLinkUrl": artist_url
-                            },
-                            {
-                                "action": "message",
-                                "label": "최근 발매 노래 검색",
-                                "messageText": "노래정보를 알려줄게"
-                            },
+                            }
 
                         ]
                     }
-                }
+                },
+                #3.list 카드형 : 아티스트의
+                {
+                    "listCard": {
+                        "header": {
+                            "title": name+"의 노래를 찾았습니다"
+                        },
+                        # get_top_tracks는 아티스트의 id를 이용하여 DynamoDB나 API에서 해당 아티스트의 탑 트랙을 찾는 함수
+                        # ListCard 형태에 맞게 리턴
+                        "items": track_result,
+                        "buttons": [
+                            {
+                                "label": "Youtube에서 검색하기",
+                                "action": "webLink",
+                                "webLinkUrl": youtube_url
+                            }
+                        ]
+                    }
+                },
+
             ]
         }
     }
     return result
 
+#1.simple text형식 : DB에 없는 아티스트 입력했을 때 메세지
 def response_insert():
-    #1.simple text형식 : 메세지만 나갈떄
     result = {
         "version": "2.0",
         "template": {
@@ -194,17 +207,41 @@ def get_toptracks(artist_id,artist_name,headers):
 
     print("insert dynamodb 성공")
 
+def get_toptracks_db(id):
+    #dynanoDB 파티션키 : track_id
+    #track_result=table.query(KeyConditionExpression=Key('artist_id').eq(id))
+    select_result = table.scan(FilterExpression = Attr('artist_id').eq(id))
+    #print(track_result)
 
-def lambda_handler(event):
+    #최근 발매된 앨범순으로 정렬
+    select_result['Items'].sort(key=lambda x: x['album']['release_date'], reverse=True)
+    items = []
+    #최근 발매된 3개만
+    for track in select_result['Items'][:3]:
+
+        # ListCard 형태에 맞게 리턴
+        temp_dic = {
+            "title": track['track_name'], #타이틀곡명
+            "description": track['album']['release_date'], #발매일
+            "imageUrl": track['album']['album_image'], #앨범커버이미지
+            "link": {
+                "web": track['track_url'] #스포티파이링크
+            }
+        }
+
+        items.append(temp_dic)
+
+    return items
+
+
+def lambda_handler(event,context):
 
     # 메시지 내용은 request의 ['body']에 들어 있음
-    #request_body = json.loads(event['body'])
-    #print(request_body)
-    #params = request_body['action']['params']
-    params = event['action']['params']
+    request_body = json.loads(event['body'])
+    print(request_body)
 
-    #group = params['group'] # 그룹아티스트 파라미터
-    input_artist = event['userRequest']['utterance']
+    #group = request_body['action']['params']['group'] # 그룹아티스트 파라미터
+    input_artist = request_body['userRequest']['utterance']
 
     #DB에 있으면,mysql에 검색
     select_query="SELECT * from artists where artist_name ='{}'".format(input_artist)
@@ -214,20 +251,17 @@ def lambda_handler(event):
     #db에 있는 아티스트일경우, select결과 리턴
     if len(artist_result)>0:
         id,name,followers,popularity,artist_url,image_url = artist_result[0]
-        #dynanoDB 파티션키 : track_id
-        #track_result=table.query(KeyConditionExpression=Key('artist_id').eq(id))
-        #track_result = table.scan(FilterExpression = Attr('artist_id').eq(id))
-        #print(track_result)
-
+        track_result=get_toptracks_db(id)
         #메세지
-        message = response_select(name,followers,popularity,artist_url,image_url)
+        message = response_select(name,followers,popularity,artist_url,image_url,track_result)
 
     #db에 없는 아티스트일경우,
     else:
-        artist_id = get_artist(input_artist,get_header())
-        #get_toptracks(artist_id,input_artist,get_header())
         message=response_insert()
+        artist_id = get_artist(input_artist,get_header())
+        get_toptracks(artist_id,input_artist,get_header())
 
+    print("최종전송 msg")
     print(message)
 
     return {
@@ -237,67 +271,3 @@ def lambda_handler(event):
             'Access-Control-Allow-Origin': '*',
         }
     }
-
-
-#테스트코드
-event={
-    "bot":{
-        "id":"60b628c87e223a78e8750a68!",
-        "name":"스포티파이 검색 봇"
-    },
-    "intent":{
-        "id":"60bd22f6a0293f36984913ef",
-        "name":"국내아티스트명 블록 ",
-        "extra":{
-            "reason":{
-                "code":1,
-                "message":"OK"
-            }
-        }
-    },
-    "action":{
-        "id":"60bc72c24e460e6c6be02a11",
-        "name":"API Gateway Server",
-        "params":{
-            "group":"BTOB"
-        },
-        "detailParams":{
-            "group":{
-                "groupName":"",
-                "origin":"BTOB",
-                "value":"BTOB"
-            }
-        },
-        "clientExtra":{
-
-        }
-    },
-    "userRequest":{
-        "block":{
-            "id":"60bd22f6a0293f36984913ef",
-            "name":"국내아티스트명 블록 "
-        },
-        "user":{
-            "id":"c3e55311e419995dfdbfac37cc496f390887539b442129dc07dbeb3a9d2420ec24",
-            "type":"botUserKey",
-            "properties":{
-                "botUserKey":"c3e55311e419995dfdbfac37cc496f390887539b442129dc07dbeb3a9d2420ec24",
-                "isFriend":'true',
-                "plusfriendUserKey":"cCFcsmWzskCa",
-                "bot_user_key":"c3e55311e419995dfdbfac37cc496f390887539b442129dc07dbeb3a9d2420ec24",
-                "plusfriend_user_key":"cCFcsmWzskCa"
-            }
-        },
-        "utterance":"BTOB",
-        "params":{
-            "surface":"Kakaotalk.plusfriend"
-        },
-        "lang":"ko",
-        "timezone":"Asia/Seoul"
-    },
-    "contexts":[
-
-    ]
-
-}
-lambda_handler(event)
